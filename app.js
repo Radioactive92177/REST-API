f66081c64c5f0a8b5d623d088be66e0961287695
MG Raju <raju.11811924@lpu.in>
2020-10-03 19:18:16 +0530

    REST API first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+app.use(bodyParser.json());
+
+// Import routes
+const postRoutes = require("./routes/posts");
+
+app.use("/posts", postRoutes);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("We are on home");
+});
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB!");
+  }
+);
+
+// Start listening
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+
+// GET BACK ALL THE POSTS
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// GET BACK A SPECIFIC POST
+router.get("/:postId", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// SUBMIT A POST
+router.post("/", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (e) {
+    res.json({ message: e });
+  }
+});
+
+// DELETE A SPECIFIC POST
+router.delete("/:postId", async (req, res) => {
+  try {
+    const removedPost = await Post.deleteOne({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+module.exports = router;
